b3304a6bbfde7e5d0fb46c59dbb03fdc4ba7aa30
gbronca <42703789+gbronca@users.noreply.github.com>
2020-09-20 13:12:55 +0100

    converted App to class

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: '',
+    }
+  }
+  render() {
+    return (
+      <div className='tc'>
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
