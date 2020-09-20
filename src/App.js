bfe094dc56d7f1035d8d2384aeeb9abb105244f2
gbronca <42703789+gbronca@users.noreply.github.com>
2020-09-20 13:30:08 +0100

    Filter robot friend by name

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ class App extends React.Component {
     super()
     this.state = {
       robots: robots,
-      searchField: '',
+      searchfield: '',
     }
   }
+
+  onSearchChange = (event) => {
+    this.setState( { searchfield: event.target.value })
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    });
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
