56c17d1bcb40043ce9ea529d20fa396a8a1ee74c
gbronca <42703789+gbronca@users.noreply.github.com>
2020-09-21 19:54:07 +0100

    Updates

---
+++ precache-manifest.51050edb811c3a25ae9367ffe30a20ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38398b39bfb872d3e4dc6d9c00e2cee6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f218c2d6a075ef03f38a",
+    "url": "/robofriends/static/css/main.92c92f72.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f218c2d6a075ef03f38a",
+    "url": "/robofriends/static/js/main.be37f504.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
